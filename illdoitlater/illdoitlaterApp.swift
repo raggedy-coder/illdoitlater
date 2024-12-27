b2eb45cae2336f124a2c23434b0f8a9b0423a738
RB de Guzman <deguzman.rb@gmail.com>
2024-12-26 18:49:21 -0600

    Initial Commit

---
+++ illdoitlater/illdoitlaterApp.swift
@@ -0,0 +1,32 @@
+//
+//  illdoitlaterApp.swift
+//  illdoitlater
+//
+//  Created by RB de Guzman on 12/26/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct illdoitlaterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
